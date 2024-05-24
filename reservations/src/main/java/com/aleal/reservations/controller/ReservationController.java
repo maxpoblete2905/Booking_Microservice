7f927ad4b683ebd23172ec3af203e1ea3d0974a9
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-05-23 22:37:57 -0400

    set service distributed traceability

--- reservations/src/main/java/com/aleal/reservations/controller/ReservationController.java
+++ reservations/src/main/java/com/aleal/reservations/controller/ReservationController.java
@@ -7,6 +7,8 @@ import com.aleal.reservations.model.PropertiesReservations;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +25,17 @@ public class ReservationController {
 	@Autowired
 	private ReservationsConfigurations configReservations;
 
+	private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
+
 	@GetMapping("reservations")
 	public List<Reservation> search(){
-		return (List<Reservation>) this.service.search();	
+		logger.info("inicio metodo search reservations");
+		return (List<Reservation>) this.service.search();
 	}
 
 	@GetMapping("/reservations/read/properties")
-	public String getPropertiesHotels() throws JsonProcessingException {
+	public String getPropertiesReservations() throws JsonProcessingException {
+		logger.info("inicio metodo getPropertiesReservations");
 		ObjectWriter owj = new ObjectMapper().writer().withDefaultPrettyPrinter();
 		PropertiesReservations propReservations = new PropertiesReservations(configReservations.getMsg(), configReservations.getBuildVersion(),
 				configReservations.getMailDetails());
