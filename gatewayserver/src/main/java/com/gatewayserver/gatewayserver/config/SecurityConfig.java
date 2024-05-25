9359776bf7160c4fe0305d5881ebce6eeac91cef
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-05-25 17:58:49 -0400

    test keycloak

--- gatewayserver/src/main/java/com/gatewayserver/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/gatewayserver/gatewayserver/config/SecurityConfig.java
@@ -1,2 +1,41 @@
-package com.gatewayserver.gatewayserver.config;public class securityConfig {
+package com.gatewayserver.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/applications/apihotel/**").hasRole("HOTELS")
+                        .pathMatchers("/applications/apirooms/**").authenticated()
+                        .pathMatchers("/applications/apireservations/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(grantedAuthorities())))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthorities() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new RoleKeycloakConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter::convert);
+    }
+
 }
