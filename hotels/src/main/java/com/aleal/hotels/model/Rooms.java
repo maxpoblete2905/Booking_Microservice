9c46c43d6ab553742f493ac8510809b92c30578e
max esteban poblete pizarro <max.poblete2905@gmail.com>
2024-05-26 01:07:34 -0400

    adjustment docker compose

--- hotels/src/main/java/com/aleal/hotels/model/Rooms.java
+++ hotels/src/main/java/com/aleal/hotels/model/Rooms.java
@@ -1,8 +1,12 @@
 package com.aleal.hotels.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rooms {
     private long roomId;
     private long hotelId;
--- hotels/src/test/java/com/aleal/hotels/hotels/controllerIntegrationTest/ControllerIntegrationTest.java
+++ hotels/src/test/java/com/aleal/hotels/hotels/controllerIntegrationTest/ControllerIntegrationTest.java
@@ -3,15 +3,25 @@ package com.aleal.hotels.hotels.controllerIntegrationTest;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.mockito.Mockito.*;
+
+import com.aleal.hotels.model.HotelRooms;
+import com.aleal.hotels.model.Rooms;
+import com.aleal.hotels.controller.HotelController;
+import com.aleal.hotels.services.HotelServiceImpl;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -21,8 +31,30 @@ public class ControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
+    @Mock
+    private HotelServiceImpl hotelService;
+
+    @InjectMocks
+    private HotelController hotelController;
+
+    @Before
     public void setUp() {
+        HotelRooms hotel = new HotelRooms();
+        hotel.setHotelId(2);
+        hotel.setHotelName("Hotel Transilvania");
+        hotel.setHotelAddress("Calle transilvania 444");
+
+        Rooms room = new Rooms();
+        room.setRoomId(3);
+        room.setHotelId(2);
+        room.setRoomName("Pieza 101");
+        room.setRoomAvailable("No");
+
+        List<Rooms> rooms = new ArrayList<>();
+        rooms.add(room);
+        hotel.setRooms(rooms);
+
+        when(hotelService.searchHotelById(2)).thenReturn(hotel);
     }
 
     @Test
@@ -42,17 +74,16 @@ public class ControllerIntegrationTest {
 
     @Test
     public void testSearchHotelById() throws Exception {
-        mockMvc.perform(get("/hotels/1"))
+        mockMvc.perform(get("/hotels/2"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.hotelId").exists())
-                .andExpect(jsonPath("$.hotelName").exists())
-                .andExpect(jsonPath("$.hotelAddress").exists())
-                .andExpect(jsonPath("$.rooms").isArray())
-                .andExpect(jsonPath("$.rooms[0].roomId").exists())
-                .andExpect(jsonPath("$.rooms[0].hotelId").exists())
-                .andExpect(jsonPath("$.rooms[0].roomName").exists())
-                .andExpect(jsonPath("$.rooms[0].roomAvailable").exists());
+                .andExpect(jsonPath("$.hotelId").value(2))
+                .andExpect(jsonPath("$.hotelName").value("Hotel Transilvania"))
+                .andExpect(jsonPath("$.hotelAddress").value("Calle transilvania 444"))
+                .andExpect(jsonPath("$.rooms[0].roomId").value(3))
+                .andExpect(jsonPath("$.rooms[0].hotelId").value(2))
+                .andExpect(jsonPath("$.rooms[0].roomName").value("Pieza 101"))
+                .andExpect(jsonPath("$.rooms[0].roomAvailable").value("No"));
     }
 
 }
